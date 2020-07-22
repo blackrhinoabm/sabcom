dbe9267687863cf08cb73072dbea6ec486e951a5
joerischasfoort <joerischasfoort@gmail.com>
2020-07-22 11:18:17 +0200

    fix testing folder python3

--- sabcom/src/helpers.py
+++ sabcom/src/helpers.py
@@ -54,7 +54,7 @@ def confidence_interval(data, av):
     return stats.t.interval(alpha=0.95, df=24, loc=av, scale=sigma)
 
 
-def generate_district_data(number_of_agents, max_districts=None):
+def generate_district_data(number_of_agents, max_districts=None, pathstart=''):
     """
     Transforms input data on informal residential, initial infections, and population and transforms it to
     a list of organised data for the simulation.
@@ -63,10 +63,10 @@ def generate_district_data(number_of_agents, max_districts=None):
     :param max_districts: (optional) maximum amount of districts simulated, integer
     :return: data set containing district data for simulation, list
     """
-    informal_residential = pd.read_csv('input_data/Informal_Residential.csv').iloc[:-1]
-    inital_infections = pd.read_csv('input_data/Cases_With_Subdistricts.csv', index_col=0)
+    informal_residential = pd.read_csv('{}input_data/Informal_Residential.csv'.format(pathstart)).iloc[:-1]
+    inital_infections = pd.read_csv('{}input_data/Cases_With_Subdistricts.csv'.format(pathstart), index_col=0)
     inital_infections = inital_infections.sort_index()
-    population = pd.read_csv('input_data/population.csv')
+    population = pd.read_csv('{}input_data/population.csv'.format(pathstart))
 
     # normalise district informality
     x = informal_residential[['Informal_residential']].values.astype(float)
